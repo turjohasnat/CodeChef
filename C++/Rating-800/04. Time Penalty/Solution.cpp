e803bd88c7bf40a00102c3730891923455974941
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-18 11:35:12 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/04. Time Penalty/Solution.cpp
@@ -0,0 +1,31 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll x, y;
+    cin >> x >> y;
+
+    ll result = x + y * 10;
+    cout << result << endl;
+    
+    return 0;
+}
