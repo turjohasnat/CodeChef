ee8344e20647dc64ff85880eab3231a3cd677e45
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-18 19:15:38 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/26. Chef Bakes Cake 2/Solution.cpp
@@ -0,0 +1,50 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n);
+        for(auto& i : a)
+            cin >> i;
+
+        vector<ll> b(n);
+        for(auto& i : b)
+            cin >> i;
+
+        ll total_cost = 0;
+        for(ll i = 0; i < n; i++){
+            if(a[i] > b[i])
+                total_cost += (a[i] - b[i]); 
+        }
+
+        cout << total_cost << el;
+        
+    }
+    
+    return 0;
+}
