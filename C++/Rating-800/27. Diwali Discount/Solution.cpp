d2ed386a3ce2a94ba25becf4f1927865c7de9216
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-18 19:21:24 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/27. Diwali Discount/Solution.cpp
@@ -0,0 +1,34 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a, b;
+    cin >> a >> b;
+
+    ll result = a - b;
+    if(result < 0)
+        cout << 0 << el;
+    else
+        cout << result << el;
+    
+    return 0;
+}
