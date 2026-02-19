10d5c577f5709b2833f20e1ed2e5375a7dc232fb
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-19 14:06:38 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/42. Best Movie/Solution.cpp
@@ -0,0 +1,51 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        ll cost = INT16_MAX;
+        bool isCheck = false;
+        for(ll i = 0; i < n; i++){
+            ll a, b;
+            cin >> a >> b;
+
+            if(a >= 7){
+                cost = min(cost, b);
+                isCheck = true;
+            }
+        }
+
+        if(!isCheck)
+            cout << -1 << el;
+        else
+            cout << cost << el;
+        
+    }
+    
+    return 0;
+}
