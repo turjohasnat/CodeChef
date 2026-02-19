7b10e37a1ac0d1da7ff3d92a5626e25a11f22584
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-19 11:13:40 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/34. Move Grid/Solution.cpp
@@ -0,0 +1,32 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    ll x = a - c;
+    ll y = b - d;
+    cout << x << " " << y << el;
+        
+    return 0;
+}
