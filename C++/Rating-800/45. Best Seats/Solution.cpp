39c11feb12c406045ce37e67add28ad2f5733d55
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-19 19:59:41 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/45. Best Seats/Solution.cpp
@@ -0,0 +1,46 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n);
+        for(auto& i : a)
+            cin >> i;
+
+        ll cost = LONG_MAX;
+        for(ll i = 0; i < n-1; i++){
+            ll b = a[i] + a[i+1];
+
+            cost = min(cost, b);
+        }
+        
+        cout << cost << el;
+    }
+    
+    return 0;
+}
