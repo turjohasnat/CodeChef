319f98abebc5b560c8b959c33d52700e17c67160
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-19 21:15:42 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/49. Payment Scheme/Solution.cpp
@@ -0,0 +1,35 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll x;
+    cin >> x;
+
+    ll total_pay = 100 + 4 * x;
+
+    if(total_pay > 300)
+        cout << 300 << el;
+    else
+        cout << total_pay << el;
+    
+    return 0;
+}
