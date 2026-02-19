7fa812dd502d659e5b7fcf095a35f8fced8a4bce
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-19 14:12:46 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/43. Chef and Socks/Solution.cpp
@@ -0,0 +1,34 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a, x, y;
+    cin >> a >> x >> y;
+
+    ll result = x + y;
+    if(result >= a)
+        cout << "YES" << el;
+    else
+        cout << "NO" << el;
+    
+    return 0;
+}
