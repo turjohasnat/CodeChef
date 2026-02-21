d9d3a2c7b8b145857c89435e4c0f3464a6c2f9d4
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-21 20:19:39 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/06. Triplet Maximization/Solution.cpp
@@ -0,0 +1,45 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll x, y;
+        cin >> x >> y;
+
+        ll sum = (x + y)/3;
+        
+        ll result;
+        if(sum < y)
+            result = sum * 2;
+        else{
+            ll ans = sum - y;
+            result = y * 2 + ans;
+        }
+
+        cout << result << el;
+    }
+    
+    return 0;
+}
