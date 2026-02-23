296d4a22e1baffa865c24518c646d7912a896a05
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-23 19:16:48 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/14. Time Machine/Solution.cpp
@@ -0,0 +1,35 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll x;
+    cin >> x;
+
+    ll result = x + 25;
+    
+    if(result >= 2050)
+        cout << "Yes" << el;
+    else
+        cout << "No" << el;
+    
+    return 0;
+}
