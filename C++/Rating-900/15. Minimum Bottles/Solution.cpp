3fc07501e8d754c13e62c62c97c9f14170f768c1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-23 19:35:24 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/15. Minimum Bottles/Solution.cpp
@@ -0,0 +1,46 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n, x;
+        cin >> n >> x;
+
+        vector<ll> a(n);
+        for(auto& i : a)
+            cin >> i;
+
+        ll total_water = 0;
+        for(ll i = 0; i < n; i++){
+            total_water += a[i];
+        }
+
+        ll result = ceil((dou)total_water / x);
+        cout << result << el;
+      
+    }
+    
+    return 0;
+}
