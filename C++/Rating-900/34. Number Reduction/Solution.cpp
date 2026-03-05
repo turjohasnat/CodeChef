0f3111dedd32606c14f98ed954db2f7d671d96bc
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-06 00:21:55 +0600

    Add solution for number reduction problem

---
+++ C++/Rating-900/34. Number Reduction/Solution.cpp
@@ -0,0 +1,44 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll x;
+        cin >> x;
+
+        while (x > 1){
+            if(x > 3)
+                x -= 3;
+            else if(x % 2 == 0)
+                x /= 2;
+            else
+                break;
+        }
+        
+        cout << x << el;
+    }
+    
+    return 0;
+}
