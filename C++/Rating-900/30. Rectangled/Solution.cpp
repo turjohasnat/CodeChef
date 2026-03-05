a5d567f7095385373bcdd40878888b10943f3eff
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-05 20:05:49 +0600

    Add Solution.cpp for Rectangled problem

---
+++ C++/Rating-900/30. Rectangled/Solution.cpp
@@ -0,0 +1,42 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+bool comp(int a, int b) {
+return a > b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        ll l = n / 4;
+        ll w = (n / 2) - (n / 4);
+
+        ll area = l * w;
+        cout << area << el;
+    }
+    
+    return 0;
+}
