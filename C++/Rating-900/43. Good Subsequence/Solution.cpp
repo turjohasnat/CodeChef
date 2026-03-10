8ad246f51470817eee1ea584de8693304dbf0e1e
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-11 00:46:18 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/43. Good Subsequence/Solution.cpp
@@ -0,0 +1,51 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+bool comp(int a, int b) {
+return a > b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n);
+        for(auto& i : a) cin >> i;
+
+        ll last = a[0] % 2;
+
+        ll good = 1;
+        for(ll i = 1; i < n; i++){
+            if(a[i] % 2 != last){
+                good++;
+                last = a[i] % 2;
+            }
+        }
+        
+        cout << good << el;
+    }
+
+    return 0;
+}
