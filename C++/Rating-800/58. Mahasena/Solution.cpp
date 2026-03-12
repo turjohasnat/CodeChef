35346cc8ce1c2a208850324011daeb86849904f1
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-12 21:01:11 +0600

    Create Solution.cpp

---
+++ C++/Rating-800/58. Mahasena/Solution.cpp
@@ -0,0 +1,46 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+bool comp(int a, int b) {
+return a > b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    vector<ll> a(n);
+    for(auto& i : a)
+        cin >> i;
+
+    ll even = 0, odd = 0;
+    for(ll i = 0; i < n; i++){
+        if(a[i] % 2 == 0) even++;
+        else odd++;
+    }
+
+    if(even > odd)
+        cout << "READY FOR BATTLE" << el;
+    else
+        cout << "NOT READY" << el;
+
+    return 0;
+}
