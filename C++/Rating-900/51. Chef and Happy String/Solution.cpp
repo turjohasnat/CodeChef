b8301b02b604e66dfc8da4cb7ec8cb1e7e26d81a
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-13 23:15:48 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/51. Chef and Happy String/Solution.cpp
@@ -0,0 +1,49 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        string s;
+        cin >> s;
+
+        ll vowels = 0;
+        for(ll i = 0; i < s.size(); i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') 
+                vowels++;
+            else
+                vowels = 0;
+
+            if(vowels > 2)
+                break;
+        }
+        
+        if(vowels == 0)
+            cout << "Sad" << el;
+        else
+            cout << "Happy" << el;
+    }
+
+    return 0;
+}
