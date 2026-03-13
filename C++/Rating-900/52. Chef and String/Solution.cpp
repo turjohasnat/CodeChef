1d95ca14983298298e3989f24020beba1f8524fa
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-03-13 23:29:24 +0600

    Create Solution.cpp

---
+++ C++/Rating-900/52. Chef and String/Solution.cpp
@@ -0,0 +1,49 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define el "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        string s;
+        cin >> s;
+
+        ll pair = 0;
+        for(ll i = 0; i < s.size() - 1; ){
+            if(s[i] == 'x' && s[i+1] == 'y'){
+                pair++;
+                i += 2;
+            }
+            else if(s[i] == 'y' && s[i+1] == 'x'){
+                pair++;
+                i += 2;
+            }
+            else
+                i++;
+        }
+
+        cout << pair << el;
+    }
+
+    return 0;
+}
